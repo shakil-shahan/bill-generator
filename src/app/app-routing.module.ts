01b6532b9141b534a6f3f6159e2ebf438430493c
MdShakilMAhmud <shahankhanbd@example.com>
2019-04-13 20:04:29 +0600

    html added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {MatTableModule} from '@angular/material/table';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MatTableModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export interface Transaction {
+  item: string;
+  cost: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bill-generator';
+  title = 'Bill Generator';
+
+  displayedColumns: string[] = ['item-name', 'price', 'quantity', 'sub-total'];
+  transactions: Transaction[] = [
+    {item: 'Apple', cost: 150},
+    {item: 'Banana', cost: 8},
+    {item: 'Watermelon', cost: 220},
+    {item: 'Orange', cost: 200},
+    {item: 'Jackfruit', cost: 350},
+  ];
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
 }
